3d0880325054e30e051523a2807c71ab28478855
jerome <jerome@metabolism.fr>
2018-12-19 06:52:48 -0800

    public release for archive

---
+++ doc/changelog.md
@@ -0,0 +1,25 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) 
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+#1.1.2
+- use path define in composer.json file for wordpress-core type package
+
+#1.1.1
+- Restore Symlink (with a fix for windows)
+
+#1.1.0
+- Removed Symlink (which is not supported by Windows system)
+
+### 1.0.2
+Added poast package update action
+Changed extra name
+
+### 1.0.2
+Added poast package update action
+Changed extra name
+
+### 1.0.1
+Removed composer version check
---
+++ src/Composer/Installer/PackageInstaller.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * User: Paul Coudeville <paul@metabolism.fr>
+ */
+
+namespace Rocket\Composer\Installer;
+
+use Composer\Composer,
+	Composer\IO\IOInterface,
+	Composer\Installer\LibraryInstaller,
+	Composer\Package\PackageInterface;
+
+class PackageInstaller extends LibraryInstaller
+{
+    protected $types = [
+        'wordpress-core'
+    ];
+
+    /**
+     * Return correct install path
+     * @param PackageInterface $package
+     * @return string
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+    	$extra = $this->composer->getPackage()->getExtra();
+
+    	if( isset($extra['installer-paths']) )
+	    {
+	    	foreach ( $extra['installer-paths'] as $path=>$types)
+		    {
+			    if( in_array('type:'.$package->getType(), $types) )
+			    	return $path;
+		    }
+	    }
+
+        return parent::getInstallPath($package);
+    }
+
+    /**
+     * Evaluate if package will be processed
+     *
+     * @param string $packageType
+     * @return bool
+     */
+    public function supports($packageType)
+    {
+        return in_array($packageType, $this->types);
+    }
+
+}
